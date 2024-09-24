ef41b47bbe8f47db68308c9d73b69fbf4c341a0d
Veronika Levchuk <131522998+adAstra97@users.noreply.github.com>
2024-09-24 19:25:48 +0300

    Feat: add categories logics & update pizza item(#3)
    * feat: add logo
    
    * feat: add active categories
    
    * feat: add pizza item data

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,12 +1,33 @@
-export const Categories = () => (
-  <div className="categories">
-    <ul>
-      <li className="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-    </ul>
-  </div>
-);
+import { useState } from 'react';
+
+const caterogies: string[] = [
+  'Все',
+  'Мясные',
+  'Вегетарианская',
+  'Гриль',
+  'Острые',
+  'Закрытые',
+];
+
+export const Categories = () => {
+  const [activeCategory, setActiveCategory] = useState<number>(0);
+
+  const handleActiveCategory = (index: number): void => {
+    setActiveCategory(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {caterogies.map((category, index) => (
+          <li
+            onClick={() => handleActiveCategory(index)}
+            key={index}
+            className={activeCategory === index ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
