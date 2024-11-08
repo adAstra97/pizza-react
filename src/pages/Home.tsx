07fc3ffc7da2d593d5a76fac5ec391f749fd4fc2
Nika <131522998+adAstra97@users.noreply.github.com>
2024-11-08 15:24:11 +0300

    Feat: add store for filtering (#11)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,18 @@
 import { useContext, useEffect, useState } from 'react';
 import { Categories } from '../components/Categories/Categories';
 import { Sort } from '../components/Sort/Sort';
-import { IPizza, ISortType } from '../models/pizza.model';
+import { IPizza } from '../models/pizza.model';
 import { Skeleton } from '../components/PizzaItem/Skeleton';
 import PizzaItem from '../components/PizzaItem/PizzaItem';
 import { SearchContext } from '../context/SearchContext';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../redux/store';
+import { setCategoryId } from '../redux/slices/filterSlice';
 
 export const Home: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { categoryId, sort } = useSelector((state: RootState) => state.filter);
   const [pizzasData, setPizzasData] = useState<IPizza[]>([]);
-  const [activeCategory, setActiveCategory] = useState<number>(0);
-  const [activeSortType, setSortType] = useState<ISortType>({
-    name: 'популярности',
-    sortBy: 'rating',
-  });
   const [isAscending, setIsAscending] = useState<boolean>(true);
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -25,7 +25,7 @@ export const Home: React.FC = () => {
       try {
         setIsLoading(true);
         const response = await fetch(
-          `${import.meta.env.VITE_API_URL}?category=${activeCategory}&sortBy=${activeSortType.sortBy}&order=${isAscending ? 'asc' : 'desc'}`,
+          `${import.meta.env.VITE_API_URL}?category=${categoryId}&sortBy=${sort.sortBy}&order=${isAscending ? 'asc' : 'desc'}`,
         );
 
         if (response.status === 404) {
@@ -51,7 +51,7 @@ export const Home: React.FC = () => {
     };
 
     fetchData();
-  }, [activeCategory, activeSortType, isAscending]);
+  }, [categoryId, sort, isAscending]);
 
   const pizzas = pizzasData
     .filter(pizza => {
@@ -67,15 +67,10 @@ export const Home: React.FC = () => {
     <div className="container">
       <div className="content__top">
         <Categories
-          activeCategory={activeCategory}
-          setActiveCategory={(id: number) => setActiveCategory(id)}
+          activeCategory={categoryId}
+          setActiveCategory={(id: number) => dispatch(setCategoryId(id))}
         />
-        <Sort
-          activeSortType={activeSortType}
-          setSortType={(sort: ISortType) => setSortType(sort)}
-          isAscending={isAscending}
-          setIsAscending={setIsAscending}
-        />
+        <Sort isAscending={isAscending} setIsAscending={setIsAscending} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
